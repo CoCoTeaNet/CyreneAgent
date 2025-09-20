ca88a18d3118e117c0d7a935f7f2226250cc6a29
CoCoTea <572315466@qq.com>
2025-09-20 19:42:25 +0800

    feat: create Project template

---
+++ composeApp/src/jvmMain/kotlin/net/cocotea/cyreneagent/Platform.kt
@@ -0,0 +1,7 @@
+package net.cocotea.cyreneagent
+
+class JVMPlatform {
+    val name: String = "Java ${System.getProperty("java.version")}"
+}
+
+fun getPlatform() = JVMPlatform()
---
+++ composeApp/src/jvmMain/kotlin/net/cocotea/cyreneagent/main.kt
@@ -0,0 +1,13 @@
+package net.cocotea.cyreneagent
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "CyreneAgent",
+    ) {
+        App()
+    }
+}
